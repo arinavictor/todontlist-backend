86b54035870c50595a957948f8db12d7d57b38b9
arinavictor <arinavictor@yahoo.com>
2019-11-12 11:29:30 -0700

    add todo routes

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,14 @@
+class TodosController < ApplicationController
+
+    def index
+        @todos =Todo.all
+
+        render json: @todos 
+    end     
+
+    def show 
+        @todo = Todo.find(params[:id])
+
+        render json: @todo
+    end 
+end
