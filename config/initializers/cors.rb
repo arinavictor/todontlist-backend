2c20ee018d4d2da33bca0475952ba6754bd0e175
arinavictor <arinavictor@yahoo.com>
2019-11-21 14:50:06 -0700

    updates cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://todontlist.firebaseapp.com/'
+    origins 'https://todontlist.firebaseapp.com'
 
     resource '*',
       headers: :any,
